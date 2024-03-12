b946c6ff5121c80dcc36b8ae2c88b11f4ae12edf
97Mith <136820655+97Mith@users.noreply.github.com>
2024-03-12 17:20:48 -0300

    Refactor: Domain: Entities: instanciando Bedroom

--- IomarInn.Domain/Entities/Bedroom.cs
+++ IomarInn.Domain/Entities/Bedroom.cs
@@ -1,3 +1,5 @@
+using IomarInn.Domain.Validation;
+
 namespace IomarInn.Domain.Entities;
 
 public sealed class Bedroom
@@ -8,4 +10,50 @@ public sealed class Bedroom
     public int Capacity { get; private set; }
     public decimal Price { get; private set; }
     public decimal Discount { get; private set; }
+
+    public Bedroom(
+        int id, List<int> guestsIds, int companyId,
+        int capacity,
+        decimal price,
+        decimal discount
+        )
+    {
+        DomainExceptionValidation
+            .When(
+                hasError: id < 0, 
+                error: "ID cannot be negative." 
+            );
+        
+        DomainExceptionValidation
+            .When(
+                hasError: capacity < 1,
+                error: "Capacity cannot be zero or negative."
+            );
+        
+        DomainExceptionValidation
+            .When(
+                hasError: guestsIds.Count > capacity,
+                error: "Capacity overflow."
+            );
+        
+        DomainExceptionValidation
+            .When(
+                hasError: price < 0,
+                error: "Price cannot be negative."
+            );
+        
+        DomainExceptionValidation
+            .When(
+                hasError: discount > price,
+                error: "Discount must be less than price."
+            );
+        
+        BedroomNumber = id;
+        GuestsIds = new List<int>(guestsIds);
+        CompanyId = companyId;
+        Capacity = capacity;
+        Price = price;
+        Discount = discount;
+
+    }
 }
