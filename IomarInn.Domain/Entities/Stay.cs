c4d734973e0e2c18910703363a49307c8cedbca8
97Mith <136820655+97Mith@users.noreply.github.com>
2024-03-13 23:30:55 -0300

    Refactor: Domain: Stay

--- IomarInn.Domain/Entities/Stay.cs
+++ IomarInn.Domain/Entities/Stay.cs
@@ -1,7 +1,11 @@
+using IomarInn.Domain.ValueObjects;
+
 namespace IomarInn.Domain.Entities;
 
 public sealed class Stay : EntityBase
 {
+    public int LaundryId { get; set; }
     public Bedroom Bedroom { get; private set; }
-    
+    public List<Product> Products { get; private set; }
+    //TODO criar VO para checkin e checkout
 }
